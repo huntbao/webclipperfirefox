ca41974596be073e9d24c32a1a1e4f40c51ba593
huntbao <gzooler@gmail.com>
2012-09-21 15:38:37 +0800

    add file

---
+++ src/chrome/content/js/mknote.namespace.js
@@ -0,0 +1,9 @@
+﻿//@huntbao @mknote
+//All right reserved
+;(function(){
+    if(typeof MKNoteWebclipper == 'undefined'){
+        MKNoteWebclipper = {}; 
+    }
+    MKNoteWebclipper.jQuery = jQuery.noConflict(true);
+})();
+
